465c7d9932bab81ded2cf567b6bee08b0bcba36e
thatpranav <jawajipranav@gmail.com>
2020-03-03 15:44:13 +0530

    added card components and css

--- src/App.js
+++ src/App.js
@@ -1,76 +1,156 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import MUIDataTable from 'mui-datatables'
+import React, { Component } from "react";
+import axios from "axios";
+import MUIDataTable from "mui-datatables";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data:[],
-      columns: ["Province/State", "Country/Region", "Total cases reported", "Changes since last day"],
+      data: [],
+      columns: [
+        {
+          name: "Province/State",
+          label: "Province/State",
+          options: {
+            filter: true,
+            sort: true
+          }
+        },
+        {
+          name: "Country/Region",
+          label: "Country/Region",
+          options: {
+            filter: true,
+            sort: true
+          }
+        },
+        {
+          name: "Total cases reported",
+          label: "Total cases reported",
+          options: {
+            filter: false,
+            sort: false
+          }
+        },
+        {
+          name: "Changes since last day",
+          label: "Changes since last day",
+          options: {
+            filter: false,
+            sort: false
+          }
+        }
+      ],
+      options: {
+        selectableRows: "none",
+        print: false,
+        filter: false,
+        download: false,
+        viewColumns: false
+      },
       totalCasesReported: 0,
-      newCasesSinceLastDay: 0
-    }
+      newCasesToday: 0
+    };
   }
   render() {
     return (
       <div className="App">
-        <h1>{this.state.totalCasesReported}</h1>
-        <h2>Total cases reported as of today</h2>
-        <h5>New cases reported since previous day: {this.state.newCasesSinceLastDay}</h5>
-        <MUIDataTable 
-        columns = {this.state.columns}
-        data = {this.state.data.slice(1)}
+        <div className="App-title">
+          <h2>Coronavirus Tracker Application</h2>
+        </div>
+        <Card className="Card">
+          <CardActionArea>
+            <CardContent className="Card-action">
+              <Typography
+                className="Typo"
+                gutterBottom
+                variant="h3"
+                component="h2"
+              >
+                {this.state.totalCasesReported}
+              </Typography>
+              <Typography
+                className="Typo"
+                variant="body2"
+                color="textSecondary"
+                component="p"
+              >
+                Total cases reported as of today
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card className="Card">
+          <CardActionArea>
+            <CardContent className="Card-action">
+              <Typography
+                className="Typo"
+                variant="body2"
+                color="textSecondary"
+                component="p"
+              >
+                New cases reported today: {this.state.newCasesToday}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <MUIDataTable
+          className="Table"
+          columns={this.state.columns}
+          data={this.state.data.slice(1)}
+          options={this.state.options}
         />
       </div>
     );
   }
 
-  async componentDidMount(){
-    const virusDataURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-    try
-    {
-      const response = await axios.get(virusDataURL)
-      const rows = response.data.split('\n') //.slice(1)
-      var data = []
+  async componentDidMount() {
+    const virusDataURL =
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    try {
+      const response = await axios.get(virusDataURL);
+      const rows = response.data.split("\n"); //.slice(1)
+      var data = [];
       var totalCasesReported = [];
-      var newCasesSinceLastDay = [];
+      var newCasesToday = [];
 
       rows.forEach(element => {
-        const row = element.split(',')
+        const row = element.split(",");
 
-        const state = row[0]
-        const country = row[1]
-        
-        var sum = 0 
-        for(var i = 5; i < row.length; i++){
-          sum += Number(row[i])
+        const state = row[0];
+        const country = row[1];
+
+        var sum = 0;
+        for (var i = 5; i < row.length; i++) {
+          sum += Number(row[i]);
         }
-        
-        const changesSinceLastDay = row[row.length - 1]
 
-        totalCasesReported.push(sum)
-        newCasesSinceLastDay.push(Number(changesSinceLastDay))
+        const changesSinceLastDay = row[row.length - 1];
 
-        data.push([
-          state, country, sum, changesSinceLastDay
-        ])
+        totalCasesReported.push(sum);
+        newCasesToday.push(Number(changesSinceLastDay));
 
+        data.push([state, country, sum, changesSinceLastDay]);
       });
 
       this.setState({
         data,
-        totalCasesReported: totalCasesReported.slice(1).reduce((a, b) => a + b, 0),
-        newCasesSinceLastDay: newCasesSinceLastDay.slice(1).reduce((a, b) => a + b, 0)
-      })
-      console.log(this.state.newCasesSinceLastDay)
-    } catch(e){
-      console.log(e)
+        totalCasesReported: totalCasesReported
+          .slice(1)
+          .reduce((a, b) => a + b, 0),
+        newCasesToday: newCasesToday.slice(1).reduce((a, b) => a + b, 0)
+      });
+      console.log(this.state.newCasesToday);
+    } catch (e) {
+      console.log(e);
     }
   }
 }
-
 
 export default App;
